c1b67ce939d3c6bbc4f7b039a6fa96aca305b266
idaballegaard <idabal01@easv365.dk>
2026-01-31 20:51:13 +0100

    API

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from 'express';
+import dotenvFlow from 'dotenv-flow';
+
+import routes from './routes';
+
+dotenvFlow.config();
+
+// create express application
+const app: Application = express();
+
+app.use('/api', routes);
+
+/**
+ * 
+ */
+export function startServer() {
+
+    const PORT: number = parseInt(process.env.PORT as string) || 4000;
+    app.listen(PORT, function() {
+        console.log("Server is running on port: " + PORT);
+    });
+}
