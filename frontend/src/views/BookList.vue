07ffc60fa55bf6df37ba4b1cede367b0812007e6
idaballegaard <idabal01@easv365.dk>
2026-04-02 10:55:38 +0200

    Button to BookDeail ind BookList.vue

--- frontend/src/views/BookList.vue
+++ frontend/src/views/BookList.vue
@@ -39,7 +39,10 @@
           </p>
 
           <div class="mt-4">
-            <button class="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600">
+            <button
+              @click="$router.push(`/books/${book.id}`)"
+              class="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600"
+            >
               Se detaljer
             </button>
           </div>
